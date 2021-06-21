fcf6b1772b61824b82cb1317bb8883e9397c7b80
Dorothy Ariana <arianadottie@gmail.com>
2021-06-21 13:15:30 +0300

    Add more courses

--- src/hooks/GlobalState.js
+++ src/hooks/GlobalState.js
@@ -9,7 +9,25 @@ const initialState = {
       title: "CSS",
       description: "We started front-end with HTML and CSS. With these tools we were able to build a profile website and I hope you were all able to build something that will display your work. We also looked at some free platforms where we can host our website, and then we dived into JavaScript, where we learned how to build interactive elements on our websites and used event listeners to get input values. We practised this by building a shopping list app. For Faster and more rapid app development, we had to learn learned React. React helped us write HTML and JS code together with ease. It comes with multiple libraries that makes code easier to execute. We learned about components, functions and classes. We learned how to build an interactive form with multiple components using props and state. We practiced with our calculator APP, keyboard APP and what every software developer has to build. We added APIs to help us use dummy data and store data from our websites. There’s more that we’ve learned and this is only a summary. To encourage you to try and remember what you’ve learned and if you’ve understood with little to do, this week’s task is to build a course summary.",
       
-    } 
+    },
+    {
+      id: 2,
+      title: "HTML",
+      description: "We started front-end with HTML and CSS. With these tools we were able to build a profile website and I hope you were all able to build something that will display your work. We also looked at some free platforms where we can host our website, and then we dived into JavaScript, where we learned how to build interactive elements on our websites and used event listeners to get input values. We practised this by building a shopping list app. For Faster and more rapid app development, we had to learn learned React. React helped us write HTML and JS code together with ease. It comes with multiple libraries that makes code easier to execute. We learned about components, functions and classes. We learned how to build an interactive form with multiple components using props and state. We practiced with our calculator APP, keyboard APP and what every software developer has to build. We added APIs to help us use dummy data and store data from our websites. There’s more that we’ve learned and this is only a summary. To encourage you to try and remember what you’ve learned and if you’ve understood with little to do, this week’s task is to build a course summary.",
+      
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      description: "We started front-end with HTML and CSS. With these tools we were able to build a profile website and I hope you were all able to build something that will display your work. We also looked at some free platforms where we can host our website, and then we dived into JavaScript, where we learned how to build interactive elements on our websites and used event listeners to get input values. We practised this by building a shopping list app. For Faster and more rapid app development, we had to learn learned React. React helped us write HTML and JS code together with ease. It comes with multiple libraries that makes code easier to execute. We learned about components, functions and classes. We learned how to build an interactive form with multiple components using props and state. We practiced with our calculator APP, keyboard APP and what every software developer has to build. We added APIs to help us use dummy data and store data from our websites. There’s more that we’ve learned and this is only a summary. To encourage you to try and remember what you’ve learned and if you’ve understood with little to do, this week’s task is to build a course summary.",
+      
+    },
+    {
+      id: 4,
+      title: "React",
+      description: "We started front-end with HTML and CSS. With these tools we were able to build a profile website and I hope you were all able to build something that will display your work. We also looked at some free platforms where we can host our website, and then we dived into JavaScript, where we learned how to build interactive elements on our websites and used event listeners to get input values. We practised this by building a shopping list app. For Faster and more rapid app development, we had to learn learned React. React helped us write HTML and JS code together with ease. It comes with multiple libraries that makes code easier to execute. We learned about components, functions and classes. We learned how to build an interactive form with multiple components using props and state. We practiced with our calculator APP, keyboard APP and what every software developer has to build. We added APIs to help us use dummy data and store data from our websites. There’s more that we’ve learned and this is only a summary. To encourage you to try and remember what you’ve learned and if you’ve understood with little to do, this week’s task is to build a course summary.",
+      
+    }    
 
   ]
 };
